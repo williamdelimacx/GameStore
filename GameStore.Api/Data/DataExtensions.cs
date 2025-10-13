fe80b7ba193e268bf38c021df828fe760a33d8a8
William de Lima <williamdelimacx@gmail.com>
2025-10-13 19:03:01 -0300

    migrations: using the entity framework repository

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -9,5 +10,14 @@ public static class DataExtensions
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connString = configuration.GetConnectionString("GameStoreContext");
+
+        services.AddSqlServer<GameStoreContext>(connString)
+                .AddScoped<IGamesRepository, EntityFrameworkGamesRepository>();
+        return services;
     }
 }
