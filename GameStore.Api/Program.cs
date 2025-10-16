6bdbe76decf56bd08e118ea3cb89584ca7d1158a
William de Lima <williamdelimacx@gmail.com>
2025-10-16 16:19:50 -0300

    feat: adding a middleware via a request delegate

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using GameStore.Api.Authorization;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
@@ -9,6 +10,28 @@ builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddGameStoreAuthorization();
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+  var stopWatch = new Stopwatch();
+
+  try
+  {
+    stopWatch.Start();
+    await next(context);
+  }
+  finally
+  {
+    stopWatch.Stop();
+
+    var elapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+    app.Logger.LogInformation(
+        "{RequestMethod} {RequestPath} request took {EllapsedMilliseconds}ms to complete",
+        context.Request.Method,
+        context.Request.Path,
+        elapsedMilliseconds);
+  }
+});
 
 await app.Services.InitializeDbAsync();
 
